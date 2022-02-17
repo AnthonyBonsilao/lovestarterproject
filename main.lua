cb9b60b2a53b6f5149d70d114ec5b8d98500014d
Cheese <bonsilaoanthony@gmail.com>
2022-02-17 16:34:34 -0500

    circle game

--- main.lua
+++ main.lua
@@ -1,25 +1,58 @@
-mouseX = 0
-mouseY = 0
-circlePosX = 250
-circlePosY = 250
-dist = 0
-circleMode = "line"
-radius = 100
+--[[
+  The game idea is to click the circles as they randomly appear:
+  [X] Spawn circles randomly around the screen
+  [X] When user clicks circle, remove from screen
+  [] Add score when user clicks a circle
+  [] Add a score element/text on screen
+  [] Add a round system that will add more circles
+  [] Add a timer that will count down, indicating time to click circles
+]]
 
-function love.draw()
-    love.graphics.print("XPos: " .. mouseX .. "\n YPos:" .. mouseY .. "\n Dist:" .. dist .. "\n" .. circleMode, 0, 0)
-    love.graphics.circle(circleMode, circlePosX, circlePosY, radius)
+
+function love.load()
+  circleCount = 100
+  mouseData = {}
+
+  mouseData.x = 0
+  mouseData.y = 0
+  mouseData.distToCirc = 0
+  mouseData.isDown = false
+  
+  circles = {}
+  for i=0,circleCount do
+    local circle = {}
+    circle.x = math.random(0, 500)
+    circle.y = math.random(0, 500)
+    circle.rad = math.random(25, 50)
+    circle.mode = "line"
+    table.insert(circles, circle)
+  end
 end
 
 function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
+
+function love.draw()
+  for i, circle in ipairs(circles) do
+    love.graphics.circle(circle.mode, circle.x, circle.y, circle.rad)
+  end
+end
+
+
 function love.update()
-    mouseX = love.mouse.getX();
-    mouseY = love.mouse.getY();
-    dist = math.dist(mouseX, mouseY, circlePosX, circlePosY);
-    if (dist < radius) then
-        circleMode = "fill"
-    else 
-        circleMode = "line"
+    mouseData.x, mouseData.y = love.mouse.getPosition()
+    mouseData.isDown = love.mouse.isDown(1)
+    
+    for i, circle in ipairs(circles) do
+        mouseData.distToCirc = math.dist(mouseData.x, mouseData.y, circle.x, circle.y)
+        if (mouseData.isDown == true and mouseData.distToCirc < circle.rad) then
+          table.remove(circles, i)
+        end
+        if (mouseData.distToCirc < circle.rad) then
+          circle.mode = "fill"
+        else 
+          circle.mode = "line"
+        end
     end
+
 end
