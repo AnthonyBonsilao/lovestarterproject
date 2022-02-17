1668afa514bfe4a81db3c7cdb7ca14539a362dc8
Cheese <bonsilaoanthony@gmail.com>
2022-02-17 18:21:32 -0500

    more functionality

--- main.lua
+++ main.lua
@@ -2,24 +2,56 @@
   The game idea is to click the circles as they randomly appear:
   [X] Spawn circles randomly around the screen
   [X] When user clicks circle, remove from screen
-  [] Add score when user clicks a circle
-  [] Add a score element/text on screen
-  [] Add a round system that will add more circles
-  [] Add a timer that will count down, indicating time to click circles
+  [X] Add score when user clicks a circle
+  [X] Add a score element/text on screen
+  [X] Add a round system that will add more circles
+  [X] Add a timer that will count down, indicating time to click circles
 ]]
 
 
 function love.load()
-  circleCount = 100
-  mouseData = {}
+  game = {}
+  game.circleCount = 10
+  game.score = 0
+  game.timer = 15
+  game.round = 1
+  game.restart = function()
+      game.circleCount = 10
+      game.timer = 15
+      game.score = 0
+      game.round = 1
+      createCircles(game.circleCount)
+  end
+  game.newRound = function()
+    game.circleCount = game.circleCount + 10
+    game.timer = 15
+    if (game.round == 1) then
+      game.timer = 14
+    elseif (game.round == 2) then
+      game.timer = 10
+    elseif (game.round == 3) then
+      game.timer = 6
+    elseif (game.round == 4) then
+      game.timer = 4
+    else
+      game.restart()
+    end
+    createCircles(game.circleCount)
+  end
 
+  
+  mouseData = {}
   mouseData.x = 0
   mouseData.y = 0
   mouseData.distToCirc = 0
   mouseData.isDown = false
   
   circles = {}
-  for i=0,circleCount do
+  createCircles(game.circleCount)
+end
+
+function createCircles(circleCount)
+  for i=1,circleCount do
     local circle = {}
     circle.x = math.random(0, 500)
     circle.y = math.random(0, 500)
@@ -33,9 +65,10 @@ function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
 
 function love.draw()
-  for i, circle in ipairs(circles) do
-    love.graphics.circle(circle.mode, circle.x, circle.y, circle.rad)
-  end
+    love.graphics.print('Time: ' .. math.floor(game.timer) .. "\n Score: " .. game.score .. '\n Round: ' .. game.round .. "\n CircleCount:" .. game.circleCount, 0, 0)
+    for i, circle in ipairs(circles) do
+      love.graphics.circle(circle.mode, circle.x, circle.y, circle.rad)
+    end
 end
 
 
@@ -43,10 +76,21 @@ function love.update()
     mouseData.x, mouseData.y = love.mouse.getPosition()
     mouseData.isDown = love.mouse.isDown(1)
     
+    if (game.timer >= 0) then
+      game.timer = game.timer - love.timer.getDelta()
+    end
+    
+    if (game.circleCount <= 0 and game.timer > 0) then
+      game.round = game.round + 1
+      game.newRound()
+    end
+    
     for i, circle in ipairs(circles) do
         mouseData.distToCirc = math.dist(mouseData.x, mouseData.y, circle.x, circle.y)
         if (mouseData.isDown == true and mouseData.distToCirc < circle.rad) then
           table.remove(circles, i)
+          game.score = game.score + 1
+          game.circleCount = game.circleCount - 1
         end
         if (mouseData.distToCirc < circle.rad) then
           circle.mode = "fill"
@@ -54,5 +98,4 @@ function love.update()
           circle.mode = "line"
         end
     end
-
 end
